2bf4a9693906d5851c2880502459c03a3001dc34
jmalochee <jmalochee@gmail.com>
2017-09-15 16:48:44 -0400

    family views--preliminary

---
+++ app/controllers/families_controller.rb
@@ -0,0 +1,32 @@
+class FamiliesController < ApplicationController
+
+  def index
+    @families = Families.all
+  end
+
+  def new
+    @family = Family.new
+  end
+
+  def create
+    @family = Family.new(family_params)
+
+    if @family.save
+      flash[:notice] = "Family added successfully"
+      redirect_to family_path(@family)
+    elsif
+      flash[:alert] = "no dice, try again."
+      render :new
+    end
+  end
+
+  def show
+    @family = Family.find(params[:id])
+  end
+
+  private
+
+  def family_params
+    params.require(:family).permit(:surname, :matriarch, :patriarch, :city, :state, :anniversary)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  
   root 'static_pages#index'
+
+  resources :families, only: [:new, :index, :show]
+  
 end
