f9b24957ac0e7f4a5d6d2e4308d84e26919cb36c
jmalochee <jmalochee@gmail.com>
2017-09-18 15:40:22 -0400

    preliminary views and routes for members

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'static_pages#index'
 
   resources :families, only: [:new, :index, :show, :create]
+  resources :members, only: [:new, :index, :show, :create]
   
 end
