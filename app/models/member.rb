ad628088b0efa047d9cd214a854675fd5bf3015a
jmalochee <jmalochee@gmail.com>
2017-09-18 15:00:58 -0400

    member model and migration

---
+++ app/models/member.rb
@@ -0,0 +1,14 @@
+class Member < ApplicationRecord
+  validates :first_name, null: false
+  validates :last_name, null: false
+  
+  NULL_ATTRS = [:mid_name, :aka, :birthdate]
+  before_save :nil_if_blank
+  
+  protected
+  
+  def nil_if_blank
+    NULL_ATTRS.each { |attr| self[attr] = nil if self[attr].blank? }
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170915181547) do
+ActiveRecord::Schema.define(version: 20170918173212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20170915181547) do
     t.string "state",       null: false
     t.string "city"
     t.date   "anniversary"
+  end
+
+  create_table "members", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name",  null: false
+    t.string "mid_name"
+    t.string "aka"
+    t.date   "birthdate"
   end
 
 end
