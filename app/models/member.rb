a1d3875bea055d82b0c857c4758f9750cbbd8650
jmalochee <jmalochee@gmail.com>
2017-09-24 18:16:39 -0400

    initial migration and model for addresses

--- app/models/member.rb
+++ app/models/member.rb
@@ -6,6 +6,7 @@ class Member < ApplicationRecord
   has_and_belongs_to_many :families, through: :families_members
   has_many :emails
   has_many :phones
+  has_many :addresses
   
   NULL_ATTRS = [:mid_name, :aka, :birthdate]
   before_save :nil_if_blank
